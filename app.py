daba8955144f00b887b880ceec0b6a8babe22a00
hypeprogrammer <goodmangil@knou.ac.kr>
2024-06-25 13:40:57 +0900

    주석으로 처리된 부분과 실제 코드 병합

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file, render_template
+from flask import Flask, request, jsonify, send_file, render_template, make_response
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -8,6 +8,14 @@ import seaborn as sns
 import io
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import warnings
+
+# 코드 경고 무시
+warnings.filterwarnings('ignore')
+
+# 한글 글꼴 설정
+plt.rc('font', family='Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False  # 한글 글꼴에서 마이너스 기호 깨지지 않도록
 
 app = Flask(__name__)
 
@@ -36,6 +44,7 @@ def load_data_and_train_model():
     return model, X_test, y_test, scaler
 
 model, X_test, y_test, scaler = load_data_and_train_model()
+
 # 시각화 함수들
 def load_data():
     california_housing = fetch_california_housing()
@@ -124,6 +133,41 @@ def visual_piechart():
     data = load_data()
     img = plot_piechart(data)
     return send_file(img, mimetype='image/png')
+
+@app.route('/visual/scatter_location')
+def scatter_plot():
+    img = io.BytesIO()
+
+    # 산점도 생성
+    housing.plot(kind='scatter', x='Longitude', y='Latitude', figsize=(8, 7), alpha=0.1)
+    plt.xlabel('경도')
+    plt.ylabel('위도')
+    plt.title('위·경도에 따른 산점도 (밀집지역)')
+
+    # 이미지를 BytesIO 객체에 저장
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.close()
+
+    # 이미지를 base64로 인코딩하여 반환
+    response = make_response(send_file(img, mimetype='image/png'))
+    response.headers['Content-Disposition'] = 'inline; filename=scatter.png'
+    return response
+
+@app.route('/visual/pairplot')
+def pairplot():
+    img = io.BytesIO()
+
+    # 페어플롯 생성
+    sns.pairplot(housing, corner=True)
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.close()
+
+    # 이미지를 base64로 인코딩하여 반환
+    response = make_response(send_file(img, mimetype='image/png'))
+    response.headers['Content-Disposition'] = 'inline; filename=pairplot.png'
+    return response
 
 # 예측을 수행하고 결과를 반환하는 함수
 @app.route('/')
